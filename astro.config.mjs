2889cb5314ed722a2b91a863737b8ceae7569976
Rex <flbaez15@gmail.com>
2026-05-20 17:52:53 -0600

    Migración a @tailwindcss/vite for astro 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://github.com/vladdbaez/',
+  site: 'https://vladdbaez.github.io',
   base: '/Portafolio',
-  integrations: [tailwind()],
   output: 'static',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
