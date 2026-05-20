becce3195f3ee24ed28f288cd3367b18a2f20ac1
Rex <flbaez15@gmail.com>
2026-05-20 17:30:10 -0600

    Configuración inicial del proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: 'https://github.com/vladdbaez/',
+  base: '/Portafolio',
+  integrations: [tailwind()],
+  output: 'static',
+});
